a5996ab76fe075b306bba35c28cafa0a2b71025c
Prasen Gope <prasen.gope@hotmail.comgit config --global user.name prasneg>
2017-04-13 12:37:55 -0400

    changes made

--- src/test/java/test/MavenTest1.java
+++ src/test/java/test/MavenTest1.java
@@ -5,14 +5,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MavenTest1 {
+	
 	@Test
 	public void Login(){
-		System.out.println("Logging into the account");
 		
+		System.out.println("Logging into the account");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
 		System.out.println("Logging into the account");
-		//driver.quit();
+	
 	}
 
 }
